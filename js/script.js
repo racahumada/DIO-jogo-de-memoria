894e803bc4bbcad8c594925fefa51892b1474f91
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-11-09 14:24:39 -0300

    Exercicio de Fixação - Concluido

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unFlipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unFlipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+cards.forEach((card) => {
+  card.addEventListener('click', flipCard);
+});
